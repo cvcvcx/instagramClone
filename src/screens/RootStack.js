991d3354a0585d324c335033a870b849af5f2206
Cho Changhun <cvcvcx@naver.com>
2022-10-13 17:11:01 +0900

    Revert "remove addTab"
    This reverts commit 15bee148a6fc39ed4c7cf7c467a2cbf00674579d.

--- src/screens/RootStack.js
+++ src/screens/RootStack.js
@@ -2,22 +2,14 @@ import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainTab from './MainTab';
-import UploadScreen from './UploadScreen';
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
-    <Stack.Navigator initialRouteName="MainTab">
+    <Stack.Navigator>
       <Stack.Screen
         name="MainTab"
         component={MainTab}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="UploadScreen"
-        component={UploadScreen}
         options={{
           headerShown: false,
         }}
